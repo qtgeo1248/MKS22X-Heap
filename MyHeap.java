0f95338ae53bcb6bf34ba6742134f7a3098540cd
qtgeo1248 <gzhou00@stuy.edu>
2019-04-23 13:58:21 -0400

    did case one child

--- MyHeap.java
+++ MyHeap.java
@@ -2,6 +2,14 @@ import java.util.Arrays;
 
 public class MyHeap {
     private static void pushDown(int[] data, int size, int index) {
-
+        if (index * 2 + 2 >= size) {
+            return; //base case
+        } else if (index * 2 + 1 == size - 1) { //case where you have one child
+            if (data[index * 2 + 1] > data[index]) { //when child is greater than parent
+                int temp = data[index];
+                data[index] = data[index * 2 + 1];
+                data[index * 2 + 1] = temp; //you can do nothing cuz you only have one child
+            }
+        }
     }
 }
