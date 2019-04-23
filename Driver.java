159b4a28900dbcaa29e51453d2bb19f32be02c0f
qtgeo1248 <gzhou00@stuy.edu>
2019-04-23 14:42:55 -0400

    testing heapify

--- Driver.java
+++ Driver.java
@@ -2,10 +2,13 @@ import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
-        int[] test1 = {19, 100, 136, 17, 20, 25, 1, 2, 7, 3, 200};
-        MyHeap.pushDown(test1, 11, 0);
+        int[] test1 = {19, 100, 136, 17, 20, 25, 1, 2, 7, 3, 200, 4, 1, 3, 4, 4};
+        /*MyHeap.pushDown(test1, 11, 0);
         System.out.println(Arrays.toString(test1));
         MyHeap.pushUp(test1, 10);
-        System.out.println(Arrays.toString(test1));
+        System.out.println(Arrays.toString(test1));*/
+
+        MyHeap.heapify(test1);
+        System.out.println(test1);
     }
 }
