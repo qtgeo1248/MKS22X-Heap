b0821c36031db51176c4b630024bfb7e69faa23d
qtgeo1248 <gzhou00@stuy.edu>
2019-04-23 14:00:04 -0400

    did one case where only one child is bigger

--- MyHeap.java
+++ MyHeap.java
@@ -17,6 +17,11 @@ public class MyHeap {
                     data[index] = data[index * 2 + 2];
                     data[index * 2 + 2] = temp;
                     pushDown(data, size, index * 2 + 2);
+                } else {
+                    int temp = data[index];
+                    data[index] = data[index * 2 + 1];
+                    data[index * 2 + 1] = temp;
+                    pushDown(data, size, index * 2 + 1);
                 }
             } else if (data[index * 2 + 2] > data[index]) {
 
